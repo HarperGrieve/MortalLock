7203a4858abd9ca2c32107d1738b635f5e58640b
Harper Grieve <harpergrieve2@gmail.com>
2021-01-31 09:36:23 -0800

    Fixed issue in loss function and refactored

--- Train-Model/Train_ML.py
+++ Train-Model/Train_ML.py
@@ -2,26 +2,22 @@ import time
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-import keras as K
-from keras import backend as k
+import keras as k
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.callbacks import TensorBoard, EarlyStopping, ModelCheckpoint
-from livelossplot import PlotLossesKeras
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from datetime import datetime
-
-# import keras.losses
 
 physical_devices = tf.config.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 current_time = str(time.time())
 
-earlyStopping = EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='min')
-mcp_save = ModelCheckpoint('../Models/Trained-Model' + current_time, save_best_only=True, monitor='val_loss',
-                           mode='auto')
+early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='min')
+save = ModelCheckpoint('../Models/Trained-Model' + current_time, save_best_only=True, monitor='val_loss',
+                       mode='auto')
 
 data = pd.read_excel('../Datasets/Full-Data-Set.xlsx')
-output = data[['Home-Team-Win', 'Away-Team-Win', 'No-Bet', 'ML-Home', 'ML-Away']]
+output = data[['Home-Team-Win', 'Away-Team-Win', 'ML-Home', 'ML-Away']]
 
 data.drop(['Score', 'Home-Team-Win', 'Away-Team-Win', 'Unnamed: 0', 'TEAM_NAME', 'Date', 'TEAM_NAME.1', 'Date.1', 'OU',
            'No-Bet', 'OU-Cover'], axis=1, inplace=True)
@@ -38,68 +34,51 @@ def keras_custom_loss_function():
     def loss(y_true, y_pred):
         win_home_team = y_true[:, 0:1]
         win_away_team = y_true[:, 1:2]
-        no_bet = y_true[:, 2:3]
-        odds_a = y_true[:, 3:4]
-        odds_b = y_true[:, 4:5]
-        # win_away_team = 1
+
+        odds_a = y_true[:, 2:3]
+        odds_b = y_true[:, 3:4]
+
         one = tf.constant(1, dtype=tf.float32)
-        neg_one = tf.constant(1, dtype=tf.float32)
-        # tf.cast(win_away_team, dtype=tf.float32)
-        gain_loss_vector = k.concatenate([win_home_team * (odds_a - neg_one) + (one - win_home_team) * neg_one,
-                                          win_away_team * (odds_b - neg_one) + (one - win_away_team) * neg_one,
-                                          k.zeros_like(odds_a)], axis=1)
-        loss = -1 * k.mean(k.sum(gain_loss_vector * y_pred, axis=1))
-        return loss
+        neg_one = tf.constant(-1, dtype=tf.float32)
+
+        gain_loss_vector = k.backend.concatenate([win_home_team * (odds_a - one) + (one - win_home_team) * neg_one,
+                                                  win_away_team * (odds_b - one) + (one - win_away_team) * neg_one
+                                                  ], axis=1)
+        return -1 * k.backend.mean(k.backend.sum(gain_loss_vector * y_pred, axis=1))
+
     return loss
 
 
-def get_model(input_dim, output_dim, base=1000, multiplier=0.25, p=0.2):
+def get_model(input_dim, output_dim, base=1000, multiplier=0.25, rate=0.2):
     inputs = tf.keras.layers.Input(shape=(input_dim,))
-    l = tf.keras.layers.BatchNormalization()(inputs)
-    l = tf.keras.layers.Dropout(p)(l)
-    n = base
-    l = tf.keras.layers.Dense(n, activation='relu')(l)
-    l = tf.keras.layers.BatchNormalization()(l)
-    l = tf.keras.layers.Dropout(p)(l)
-    n = int(n * multiplier)
-    l = tf.keras.layers.Dense(n, activation='relu')(l)
-    l = tf.keras.layers.BatchNormalization()(l)
-    l = tf.keras.layers.Dropout(p)(l)
-    n = int(n * multiplier)
-    l = tf.keras.layers.Dense(n, activation='relu')(l)
-    outputs = tf.keras.layers.Dense(output_dim, activation='softmax')(l)
-    model = tf.keras.models.Model(inputs=inputs, outputs=outputs)
-    model.compile(optimizer='Nadam', loss=keras_custom_loss_function)
-    return model
+    layer = tf.keras.layers.BatchNormalization()(inputs)
+    layer = tf.keras.layers.Dropout(rate)(layer)
+    nodes = base
+    layer = tf.keras.layers.Dense(nodes, activation='relu')(layer)
+    layer = tf.keras.layers.BatchNormalization()(layer)
+    layer = tf.keras.layers.Dropout(rate)(layer)
+    nodes = int(nodes * multiplier)
+    layer = tf.keras.layers.Dense(nodes, activation='relu')(layer)
+    layer = tf.keras.layers.BatchNormalization()(layer)
+    layer = tf.keras.layers.Dropout(rate)(layer)
+    nodes = int(nodes * multiplier)
+    layer = tf.keras.layers.Dense(nodes, activation='relu')(layer)
+    outputs = tf.keras.layers.Dense(output_dim, activation='softmax')(layer)
+    net = tf.keras.models.Model(inputs=inputs, outputs=outputs)
+    net.compile(optimizer='Nadam', loss=keras_custom_loss_function)
+    return net
 
 
 logdir = "../logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S")
-tensorboard_callback = K.callbacks.TensorBoard(log_dir=logdir)
-
+tensorboard_callback = k.callbacks.TensorBoard(log_dir=logdir)
 
 keras_custom_loss_function = keras_custom_loss_function()
-# model = get_model(106, 3, 512, 0.5, 0.7)
-# history = model.fit(train_x, train_y, validation_data=(test_x, test_y),
-#                     epochs=200, batch_size=8, callbacks=[EarlyStopping(patience=25), mcp_save,
-#                                                          tensorboard_callback])
+model = get_model(106, 2, 512, 0.5, 0.7)
+history = model.fit(train_x, train_y, validation_data=(test_x, test_y),
+                    epochs=200, batch_size=8, callbacks=[early_stopping, save,
+                                                         tensorboard_callback])
 
-
-# print('Training Loss : {}\nValidation Loss : {}'.format(model.evaluate(train_x, train_y), model.evaluate(test_x, test_y)))
-
+print(
+    'Training Loss : {}\nValidation Loss : {}'.format(model.evaluate(train_x, train_y), model.evaluate(test_x, test_y)))
 
 print('done')
-
-model = K.models.load_model('../Models/Trained-Model1612061950.5449553',
-                            custom_objects={'loss': keras_custom_loss_function})
-# print('Training Loss : {}\nValidation Loss : {}'.format(model.evaluate(train_x, train_y),
-# model.evaluate(test_x, test_y)))
-
-# test.drop(['Score', 'Home-Team-Win', 'Away-Team-Win',
-# 'Unnamed: 0', 'TEAM_NAME', 'Date', 'TEAM_NAME.1', 'Date.1', 'OU',
-#            'No-Bet', 'OU-Cover'], axis=1, inplace=True)
-# print(data[16355:16356, :])
-ml_predictions_array = [model.predict(data[16345:, :])]
-
-for x in ml_predictions_array:
-    print(x)
-    print("-------------")
--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import keras as k
 import tensorflow as tf
 
-model = k.models.load_model('Models/Trained-Model-ML')
+
+model = k.models.load_model('../Models/Trained-Model1612061950.5449553',
+                            custom_objects={'loss': keras_custom_loss_function})
+
+
+# print('Training Loss : {}\nValidation Loss : {}'.format(model.evaluate(train_x, train_y),
+# model.evaluate(test_x, test_y)))
+
+# test.drop(['Score', 'Home-Team-Win', 'Away-Team-Win',
+# 'Unnamed: 0', 'TEAM_NAME', 'Date', 'TEAM_NAME.1', 'Date.1', 'OU',
+#            'No-Bet', 'OU-Cover'], axis=1, inplace=True)
+# print(data[16355:16356, :])
+ml_predictions_array = [model.predict(data[16345:, :])]
+
+for x in ml_predictions_array:
+    print(x)
+    print("-------------")
